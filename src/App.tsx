b0a5ffdaf90b49fc25ef7827d4e90a50548e568e
Javaharlal Nehru <javaharlalnehru@yandex.ru>
2024-04-24 18:55:26 +0300

    Add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+export function ChessBoard(
+    width: number,
+    height: number,
+    whiteColor: string,
+    blackColor: string,
+    side: "white" | "black"
+ ) {
+    const board = [];
+    for (let i = 0; i < height; i++) {
+        const row = [];
+        for (let j = 0; j < width; j++) {
+            const isWhite = (i + j) % 2 === 0;
+            const color = isWhite ? whiteColor : blackColor;
+            row.push(
+                <div
+                    style={{
+                        width: '50px',
+                        height: '50px',
+                        backgroundColor: color,
+                        display: 'inline-block'
+                    }}
+                />
+            );
+        }
+        board.push(<div key={i} style={{ display: 'flex' }}>{row}</div>);
+    }
+    return <div>{side === 'white' ? board.reverse() : board}</div>;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ChessBoard(4, 5, "#fff", "#000", "white");
 }
 
 export default App;
