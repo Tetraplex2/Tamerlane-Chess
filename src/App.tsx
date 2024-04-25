0893a08a0a0db8d40226b2af1cb1d62dcc2bf9f9
Javaharlal Nehru <javaharlalnehru@yandex.ru>
2024-04-25 11:35:33 +0300

    Add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-export function ChessBoard(
-    width: number,
-    height: number,
+const WIDTH = 11;
+const HEIGHT = 10;
+
+export function ChessBoard(props: {
     whiteColor: string,
     blackColor: string,
     side: "white" | "black"
- ) {
+ }) {
     const board = [];
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < HEIGHT; i++) {
         const row = [];
-        for (let j = 0; j < width; j++) {
+        for (let j = 0; j < WIDTH; j++) {
             const isWhite = (i + j) % 2 === 0;
-            const color = isWhite ? whiteColor : blackColor;
+            const color = isWhite ? props.whiteColor : props.blackColor;
             row.push(
                 <div
                     style={{
-                        width: '50px',
-                        height: '50px',
                         backgroundColor: color,
                         display: 'inline-block'
                     }}
+                    className={`${isWhite ? "white" : "black"} square`}
                 />
             );
         }
         board.push(<div key={i} style={{ display: 'flex' }}>{row}</div>);
     }
-    return <div>{side === 'white' ? board.reverse() : board}</div>;
+    return <div className="board">{props.side === 'white' ? board.reverse() : board}</div>;
 };
 
 function App() {
-    return ChessBoard(4, 5, "#fff", "#000", "white");
+    return <ChessBoard whiteColor="#e1d3ff" blackColor="#725ac1" side="white" />;
 }
 
 export default App;
